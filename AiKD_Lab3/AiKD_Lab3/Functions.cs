7034492649e024b62b143ae6bd16278dbce8ab6f
blazej-janus <82719790+blazejjanus@users.noreply.github.com>
2021-12-05 13:38:14 +0100

    Odczyt pliku

--- AiKD_Lab3/AiKD_Lab3/Functions.cs
+++ AiKD_Lab3/AiKD_Lab3/Functions.cs
@@ -16,10 +16,47 @@ namespace AiKD_Lab3 {
                 if (File.Exists(filename) == false) {
                     throw new Exception("Podaj poprawną ścieżkę pliku źródłowego!");
                 } else {
-                    result = File.ReadAllText(filename);
+                    if (is_file_binary(filename) == true) {
+                        byte[] bytes = File.ReadAllBytes(filename);
+                        StringBuilder sb = new StringBuilder();
+                        foreach(byte b in bytes) {
+                            sb.Append(b);
+                        }
+                        result = sb.ToString();
+                    } else {
+                        result = File.ReadAllText(filename);
+                    }
                 }
             }
             return result;
+        }
+        private static bool is_file_binary(string filename) {
+            string ext = GetExtension(filename);
+            if(ext == "bin") {
+                return true;
+            }
+            if (ext == "exe") {
+                return true;
+            }
+            if (ext == "msi") {
+                return true;
+            }
+            if (ext == "dat") {
+                return true;
+            }
+            return false;
+        }
+        private static string GetExtension(string filename) {
+            string ext = null;
+            for(int i=filename.Length-1; i>0; i--) {
+                if (filename.ElementAt(i) == '.') {
+                    break;
+                } else {
+                    ext += filename.ElementAt(i);
+                }
+            }
+            ext = RotateString(ext);
+            return ext;
         }
         public static void SwapSymbols(ref CharInfo val1, ref CharInfo val2) {
             CharInfo temp_var;
